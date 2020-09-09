703d67ebc0315068679febc75a3321a870ad609f
yevhenii-b-hyuna <yevhenii.bryzhko@hyuna.bb>
2020-09-09 19:23:28 +0300

    AddFetchSettings

--- src/js/form.js
+++ src/js/form.js
@@ -1,15 +1,12 @@
 "use strict";
-
+var $ = require("jquery");
 export const useForm = () => {
-  const $form = document.getElementById("form");
   const $button = document.getElementById("form-button");
   const $overlay = document.querySelector(".reveal-overlay");
   const $name = document.getElementById("name");
   const $phone = document.getElementById("phone");
   const $html = document.getElementById("html");
-
   const form_Data = {};
-
   const showThanks = (name) => {
     const modal = document.createElement("div");
     const text = document.createElement("h2");
@@ -19,16 +16,13 @@ export const useForm = () => {
     modal.appendChild(text);
     document.body.appendChild(modal);
     modal.style.bottom = `0px`;
-
     setTimeout(() => {
       document.body.removeChild(modal);
     }, 4000);
   };
-
   document.addEventListener("click", (event) => {
     form_Data.fullName = $name.value;
     form_Data.phone = $phone.value;
-
     if (
       event.target == $button &&
       form_Data.fullName != "" &&
@@ -38,14 +32,18 @@ export const useForm = () => {
       $overlay.style.display = "none";
       $html.classList.remove("zf-has-scroll", "is-reveal-open");
       showThanks(form_Data.fullName);
-      fetch("/js/server.js", {
-        // файл-обработчик
-        method: "POST",
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded", // отправляемые данные
-        },
-        body: JSON.stringify(form_Data),
-      }).catch((error) => console.error(error));
+      const useFetch = async () => {
+        await fetch("https://serene-oasis-57731.herokuapp.com/", {
+          headers: {
+            "Content-Type": "application/json",
+          },
+
+          method: "POST",
+
+          body: JSON.stringify(form_Data),
+        }).catch((error) => console.error(error));
+      };
+      useFetch();
     }
   });
 };
